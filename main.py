121f109a1577ff6ca40a6613bc0f42cc1ea3e624
LarSyMain <sanya_sharov_2000@mail.ru>
2025-05-06 16:38:04 +0300

    modified:   Static/index.html 	modified:   Static/style.css 	modified:   main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Request, Body
-from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
+from fastapi import FastAPI, Request, Body, Response
+from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse, JSONResponse
 import random
 import asyncio
 import string
@@ -35,14 +35,6 @@ async def check_in_db(value):
         return True
     return False
     
-    
-    '''
-    data = db.chek_Short_URL()
-    result = "http://127.0.0.1:8000"+value
-    if result in data:
-        return True
-    return False
-    '''
 
 async def generate_short_url(oldURL):
     if await check_in_db(oldURL):
@@ -66,7 +58,7 @@ async def check_input(value):
         return False
 
 @app.post("/old")
-async def get_old_URL(data=Body()):
+async def post_old_URL(data=Body()):
     oldURL = data["URLOld"]
     if await check_input(oldURL) == False:
         pass
@@ -77,7 +69,15 @@ async def get_old_URL(data=Body()):
             pass
         else:
             db.add_URL(oldURL, endpointgenerate)
+            await get_new_url(endpointgenerate)
         url_mapping[endpointgenerate] = oldURL
+
+
+@app.get("/")
+async def get_new_url(data):
+    result = {"message" : "http://127.0.0.1:8000/"+str(data)}
+    return JSONResponse(content=result)
+
 
 @app.get("/{short_url}")
 async def redirictshotr(short_url: str):
